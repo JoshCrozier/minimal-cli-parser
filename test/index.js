55e34424ad9f7eb331f3a46efcd2dcadd29d0fbc
Josh Crozier <crozier4@gmail.com>
2019-01-16 20:30:51 -0500

    Add tests and implement support for shorthand flags

--- test/index.js
+++ test/index.js
@@ -27,61 +27,103 @@ describe('minimal-cli-parser', () => {
     expect(parsed).to.have.property('_').with.length(0);
   });
 
+  it('should parse a shorthand boolean flag', () => {
+    const parsed = parser(['-a']);
+
+    expect(parsed).to.have.property('a', true);
+    expect(parsed).to.have.property('_').with.length(0);
+  });
+
   it('should support multiple boolean flags', () => {
-    const parsed = parser(['--alpha', '--beta', '--gamma']);
+    const parsed = parser(['--alpha', '--beta', '--gamma', '-d']);
 
     expect(parsed).to.have.property('alpha', true);
     expect(parsed).to.have.property('beta', true);
     expect(parsed).to.have.property('gamma', true);
+    expect(parsed).to.have.property('d', true);
+    expect(parsed).to.have.property('_').with.length(0);
+  });
+
+  it('should expand shorthand flags that are grouped', () => {
+    const parsed = parser(['-abc']);
+
+    expect(parsed).to.have.property('a', true);
+    expect(parsed).to.have.property('b', true);
+    expect(parsed).to.have.property('c', true);
     expect(parsed).to.have.property('_').with.length(0);
   });
 
   it('should support flags with string values', () => {
-    const parsed = parser(['--alpha', 'value1']);
+    const parsed = parser(['--alpha', 'value1', '-b', 'value2']);
 
     expect(parsed).to.have.property('alpha', 'value1');
+    expect(parsed).to.have.property('b', 'value2');
     expect(parsed).to.have.property('_').with.length(0);
   });
 
   it('should support implicit type coercion for flags with number values', () => {
-    const parsed = parser(['--alpha', '1']);
+    const parsed = parser(['--alpha', '1', '-b', '2']);
 
     expect(parsed).to.have.property('alpha', 1);
+    expect(parsed).to.have.property('b', 2);
     expect(parsed).to.have.property('_').with.length(0);
   });
 
   it('should support decimal values', () => {
-    const parsed = parser(['--alpha', '1.0', '--beta', '2.9999', '--gamma', '3.', '--delta', '.4']);
+    const parsed = parser(['--alpha', '1.0', '--beta', '2.9999', '--gamma', '3.', '-d', '.4', '-e', '0.5']);
 
     expect(parsed).to.have.property('alpha', 1.0);
     expect(parsed).to.have.property('beta', 2.9999);
     expect(parsed).to.have.property('gamma', 3);
-    expect(parsed).to.have.property('delta', 0.4);
+    expect(parsed).to.have.property('d', 0.4);
+    expect(parsed).to.have.property('e', 0.5);
+    expect(parsed).to.have.property('_').with.length(0);
+  });
+
+  it('should set the value of the last shorthand flag in a group', () => {
+    const parsed = parser(['-abc', 'value', '--delta', '1']);
+
+    expect(parsed).to.have.property('a', true);
+    expect(parsed).to.have.property('b', true);
+    expect(parsed).to.have.property('c', 'value');
+    expect(parsed).to.have.property('delta', 1);
     expect(parsed).to.have.property('_').with.length(0);
   });
 
   it('should set the value of a flag if using an equal sign', () => {
-    const parsed = parser(['--alpha=value1', '--beta=value2']);
+    const parsed = parser(['--alpha=1', '-b=2', '--gamma=345']);
 
-    expect(parsed).to.have.property('alpha', 'value1');
-    expect(parsed).to.have.property('beta', 'value2');
+    expect(parsed).to.have.property('alpha', 1);
+    expect(parsed).to.have.property('b', 2);
+    expect(parsed).to.have.property('gamma', 345);
     expect(parsed).to.have.property('_').with.length(0);
   });
 
   it('should push command arguments to the _ property', () => {
-    const parsed = parser(['command', '--alpha', 'value1', '--beta', '--gamma=value2']);
+    const parsed = parser(['command', '--alpha', '1', '--beta', '-g', '2', '--delta=value']);
 
-    expect(parsed).to.have.property('alpha', 'value1');
+    expect(parsed).to.have.property('alpha', 1);
     expect(parsed).to.have.property('beta', true);
-    expect(parsed).to.have.property('gamma', 'value2');
+    expect(parsed).to.have.property('g', 2);
+    expect(parsed).to.have.property('delta', 'value');
     expect(parsed).to.have.property('_').that.contains('command');
   });
 
   it('should handle spacing consistently', () => {
-    const parsed = parser(['--alpha123', '--beta=value1']);
+    const parsed = parser(['-a123', '-bc456', '--beta456', '--gamma=value1', '-d=value2', '-e', '-fg90', '-x', '7', '-y=8.9', '-z', '1.2']);
 
-    expect(parsed).to.have.property('alpha123', true);
-    expect(parsed).to.have.property('beta', 'value1');
+    expect(parsed).to.have.property('a', 123);
+    expect(parsed).to.have.property('b', true);
+    expect(parsed).to.have.property('c', 456);
+    expect(parsed).to.have.property('beta456', true);
+    expect(parsed).to.have.property('gamma', 'value1');
+    expect(parsed).to.have.property('d', 'value2');
+    expect(parsed).to.have.property('e', true);
+    expect(parsed).to.have.property('f', true);
+    expect(parsed).to.have.property('g', 90);
+    expect(parsed).to.have.property('x', 7);
+    expect(parsed).to.have.property('y', 8.9);
+    expect(parsed).to.have.property('z', 1.2);
     expect(parsed).to.have.property('_').with.length(0);
   });
 });
